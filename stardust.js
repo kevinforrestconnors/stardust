510bd7ce395b7669ce9d79c64b8a05f4bc4f6abe
Kevin Connors <connorsk@grinnell.edu>
2017-10-11 19:54:08 -0500

    player moves roughly 60fps uniformly all levels now

--- stardust.js
+++ stardust.js
@@ -1,5 +1,11 @@
 
 var GLOBALS = {
+
+	lastFrameTimeMs: 0,
+	maxFPS: 60,
+	delta: 0,
+	timestep: 1000 / 60,
+
 	XYHEROwidth: 12,
 	XYHEROheight: 9,
 	
@@ -12,10 +18,7 @@ var GLOBALS = {
 } 
 
 
-//;(function () {
-	function main() {
-
-		drawMap(game.levelState);
+function updatePlayer(delta) {
 
 		if (game.keysDown.Space) {
 			if (game.player.state == "standing") {
@@ -58,6 +61,12 @@ var GLOBALS = {
 			}
 		}
 
+		if (getCurrentTile().name == "Exit Portal") {
+			game.level++;
+			game.player.pos = drawMap(levels[game.level]);
+			game.levelState = levels[game.level].slice(0, levels[game.level].length);
+			main(0);
+		}
 
 		if (game.player.state == "standing") {
 
@@ -327,377 +336,383 @@ var GLOBALS = {
 				
 			}		
 		}
+}
 
+//;(function () {
+function main(timestamp) {
+	// Throttle the frame rate
+	if (timestamp < GLOBALS.lastFrameTimeMs + (1000 / GLOBALS.maxFPS)) {
 		window.requestAnimationFrame(main);
+		return;
 	}
 
-	var canvas = document.getElementById("game");
-	var ctx = canvas.getContext("2d");
+	GLOBALS.delta += timestamp - GLOBALS.lastFrameTimeMs;
+	GLOBALS.lastFrameTimeMs = timestamp;
+
+	drawMap(game.levelState);
+
+	while (GLOBALS.delta >= GLOBALS.timestep) {
+		updatePlayer(GLOBALS.timestep);
+		GLOBALS.delta -= GLOBALS.timestep;
+	}
+
+	window.requestAnimationFrame(main);
+}
+
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
 
 
-	var mapCodes = {
-		"0": {
-			name: "Empty Space",
-			spriteX: 0,
-			spriteY: 0,
-			blocking: false,
-			eraseable: false,
-		},
-		"1": {
-			name: "Eraseable Star-wall",
-			spriteX: 2,
-			spriteY: 1,
-			blocking: true,
-			eraseable: true,
-		},
-		"2": {
-			name: "Indestructible Star-wall",
-			spriteX: 1,
-			spriteY: 1,
-			blocking: true,
-			eraseable: false,
-		},
-		"3": {
-			name: "Entrance Portal",
-			spriteX: 3,
-			spriteY: 9,
-			blocking: false,
-			eraseable: false,
-		},
-		"4": {
-			name: "Exit Portal",
-			spriteX: 3,
-			spriteY: 10,
-			blocking: false,
-			eraseable: false,
-		},
-		"5": {
-			name: "Warp Pocket",
-			spriteX: "?",
-			spriteY: "?",
-			blocking: false,
-			eraseable: false,
-		},
-		"6": {
+var mapCodes = {
+	"0": {
+		name: "Empty Space",
+		spriteX: 0,
+		spriteY: 0,
+		blocking: false,
+		eraseable: false,
+	},
+	"1": {
+		name: "Eraseable Star-wall",
+		spriteX: 2,
+		spriteY: 1,
+		blocking: true,
+		eraseable: true,
+	},
+	"2": {
+		name: "Indestructible Star-wall",
+		spriteX: 1,
+		spriteY: 1,
+		blocking: true,
+		eraseable: false,
+	},
+	"3": {
+		name: "Entrance Portal",
+		spriteX: 3,
+		spriteY: 9,
+		blocking: false,
+		eraseable: false,
+	},
+	"4": {
+		name: "Exit Portal",
+		spriteX: 3,
+		spriteY: 10,
+		blocking: false,
+		eraseable: false,
+	},
+	"5": {
+		name: "Warp Pocket",
+		spriteX: "?",
+		spriteY: "?",
+		blocking: false,
+		eraseable: false,
+	},
+	"6": {
 
-		},
-		"7": {
+	},
+	"7": {
 
-		},
-		"8": {
+	},
+	"8": {
 
-		},
-		"B": {
-			name: "Blue Magic",
-			spriteX: 2,
-			spriteY: 6,
-			blocking: true,
-			eraseable: true
-		},
-		"G": {
-			name: "Green Magic",
-			spriteX: 3,
-			spriteY: 3,
-			blocking: true,
-			eraseable: true,
-		},
-		"!": {
+	},
+	"B": {
+		name: "Blue Magic",
+		spriteX: 2,
+		spriteY: 6,
+		blocking: true,
+		eraseable: true
+	},
+	"G": {
+		name: "Green Magic",
+		spriteX: 3,
+		spriteY: 3,
+		blocking: true,
+		eraseable: true,
+	},
+	"!": {
 
+	},
+	"@": {
+
+	},
+	"#": {
+
+	}, 
+	"$": {
+
+	},
+	"(": {
+
+	},
+	")": {
+
+	},
+	"%": {
+
+	}
+}
+
+
+
+var game = {
+	level: 1,
+	levelState: [],
+	player: {
+		pos: {
+			x: 0,
+			y: 0
 		},
-		"@": {
+		direction: "right",
+		state: "standing",
+		animationStep: 0,
+		animationOffset: 0
+	},
+	keysDown: {
+		Space: false,
+		W: false,
+		A: false,
+		S: false, 
+		D: false
+	}
+}
 
-		},
-		"#": {
+function playerFall() {
+	console.log("playerFall()")
+	game.player.state = "falling";
+}
 
-		}, 
-		"$": {
+function playerStand() {
+	console.log("playerStand()")
+	game.player.state = "standing";
+	game.player.animationStep = 0;
+	game.player.animationOffset = 0;
+}
 
-		},
-		"(": {
+function playerWalkLeft() {
+	console.log("playerWalkLeft()")
+	if (game.player.state == "standing") {
 
-		},
-		")": {
+		game.player.direction = "left";	
 
-		},
-		"%": {
-
+		if (getTileBelow().blocking && !getTileLeft().blocking) {
+			game.player.state = "walkLeft"; 
 		}
 	}
+}
 
-	var levels = {
-		1: [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,2,2,0,2,0,0,0,0,0,0],
-			[0,0,0,0,1,1,2,4,0,2,1,1,0,0,0,0],
-			[0,0,0,0,0,0,2,2,2,2,0,0,0,0,0,0],
-			[0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0],
-			[0,0,0,1,0,0,0,1,1,0,0,0,1,0,0,0],
-			[0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0],
-			[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
-	}
+function playerWalkRight() {
+	console.log("playerWalkRight()")
+	if (game.player.state == "standing") {
 
-	var game = {
-		level: 1,
-		levelState: [],
-		player: {
-			pos: {
-				x: 0,
-				y: 0
-			},
-			direction: "right",
-			state: "standing",
-			animationStep: 0,
-			animationOffset: 0
-		},
-		keysDown: {
-			Space: false,
-			W: false,
-			A: false,
-			S: false, 
-			D: false
+		game.player.direction = "right";
+
+		if (getTileBelow().blocking && !getTileRight().blocking) {
+			game.player.state = "walkRight";		
 		}
 	}
+}
 
-	function playerFall() {
-		console.log("playerFall()")
-		game.player.state = "falling";
+function playerCrouch() {
+	console.log("playerCrouch()");
+	game.player.state = "crouching"
+}
+function playerMagicLeft() {
+	console.log("playerMagicLeft()")
+	if (!getTileLeft().blocking || getTileLeft().eraseable) {
+		game.player.state = "magicLeft";
 	}
-
-	function playerStand() {
-		console.log("playerStand()")
-		game.player.state = "standing";
+}
+function playerMagicRight() {
+	console.log("playerMagicRight()");
+	if (!getTileRight().blocking || getTileRight().eraseable) {
+		game.player.state = "magicRight";
+	}
+}
+function playerCrouchMagicLeft() {
+	console.log("playerCrouchMagicLeft()")
+	if (!getTileBottomLeft().blocking || getTileBottomLeft().eraseable) {
 		game.player.animationStep = 0;
 		game.player.animationOffset = 0;
+		game.player.state = "crouchMagicLeft";
 	}
+}
+function playerCrouchMagicRight() {
+	console.log("playerCrouchMagicRight()")
+	if (!getTileBottomRight().blocking || getTileBottomRight().eraseable) {
+		game.player.animationStep = 0;
+		game.player.animationOffset = 0;
+		game.player.state = "crouchMagicRight";
+	}
+}
+function playerMagicUp() {
+	console.log("playerMagicUp()");
+	if (!getTileAbove().blocking && getTileBelow().name != "Green Magic" && getTileBelow().name != "Blue Magic") {
+		game.player.state = "magicUp";
+	}
+}
+function getCurrentTile() {
+	return mapCodes[game.levelState[game.player.pos.y][game.player.pos.x]];
+}
+function getTileBelow() {
+	return mapCodes[game.levelState[game.player.pos.y + 1][game.player.pos.x]];
+}
+function getTileAbove() {
+	return mapCodes[game.levelState[game.player.pos.y - 1][game.player.pos.x]];
+}
+function getTileLeft() {
+	return mapCodes[game.levelState[game.player.pos.y][game.player.pos.x - 1]];
+}
+function getTileRight() {
+	return mapCodes[game.levelState[game.player.pos.y][game.player.pos.x + 1]];
+}
+function getTileBottomLeft() {
+	return mapCodes[game.levelState[game.player.pos.y + 1][game.player.pos.x - 1]];
+}
+function getTileBottomRight() {
+	return mapCodes[game.levelState[game.player.pos.y + 1][game.player.pos.x + 1]];
+}
+function drawTile(tileX, tileY, desX, desY, sX, sY) {
+	if (!sX) {sX = 40}
+	if (!sY) {sY = 40}
+	ctx.drawImage(sprite, tileX * sX, tileY * sY, sX, sY, desX * sX, desY * sY, sX, sY);
+}
 
-	function playerWalkLeft() {
-		console.log("playerWalkLeft()")
-		if (game.player.state == "standing") {
+function drawHero(tileX, tileY, desX, desY, sX, sY) {
+	if (!sX) {sX = 40}
+	if (!sY) {sY = 40}
+	if (game.player.direction == "right") {
+		ctx.drawImage(playerSprite, tileX * sX, tileY * sY, sX, sY, desX * sX, desY * sY, sX, sY);
+	} else {
+		ctx.drawImage(playerSprite, (11 - tileX) * sX, tileY * sY, sX, sY, desX * sX, desY * sY, sX, sY);
+	}
+	
+}
 
-			game.player.direction = "left";	
+function drawMap(map) {
 
-			if (getTileBelow().blocking && !getTileLeft().blocking) {
-				game.player.state = "walkLeft"; 
-			}
-		}
-	}
+	var i = 0, j = 0;
 
-	function playerWalkRight() {
-		console.log("playerWalkRight()")
-		if (game.player.state == "standing") {
+	for (i = 0; i < 12; i++) {
+		for (j = 0; j < 16; j++) {
+			
+			var mapCode = map[i][j];
 
-			game.player.direction = "right";
+			var tileX = mapCodes[mapCode].spriteX;
+			var tileY = mapCodes[mapCode].spriteY;
 
-			if (getTileBelow().blocking && !getTileRight().blocking) {
-				game.player.state = "walkRight";		
-			}
-		}
-	}
+			drawTile(tileX, tileY, j, i);
 
-	function playerCrouch() {
-		console.log("playerCrouch()");
-		game.player.state = "crouching"
-	}
-	function playerMagicLeft() {
-		console.log("playerMagicLeft()")
-		if (!getTileLeft().blocking || getTileLeft().eraseable) {
-			game.player.state = "magicLeft";
-		}
-	}
-	function playerMagicRight() {
-		console.log("playerMagicRight()");
-		if (!getTileRight().blocking || getTileRight().eraseable) {
-			game.player.state = "magicRight";
-		}
-	}
-	function playerCrouchMagicLeft() {
-		console.log("playerCrouchMagicLeft()")
-		if (!getTileBottomLeft().blocking || getTileBottomLeft().eraseable) {
-			game.player.animationStep = 0;
-			game.player.animationOffset = 0;
-			game.player.state = "crouchMagicLeft";
-		}
-	}
-	function playerCrouchMagicRight() {
-		console.log("playerCrouchMagicRight()")
-		if (!getTileBottomRight().blocking || getTileBottomRight().eraseable) {
-			game.player.animationStep = 0;
-			game.player.animationOffset = 0;
-			game.player.state = "crouchMagicRight";
-		}
-	}
-	function playerMagicUp() {
-		console.log("playerMagicUp()");
-		if (!getTileAbove().blocking && getTileBelow().name != "Green Magic" && getTileBelow().name != "Blue Magic") {
-			game.player.state = "magicUp";
-		}
-	}
-	function getCurrentTile() {
-		return mapCodes[game.levelState[game.player.pos.y][game.player.pos.x]];
-	}
-	function getTileBelow() {
-		return mapCodes[game.levelState[game.player.pos.y + 1][game.player.pos.x]];
-	}
-	function getTileAbove() {
-		return mapCodes[game.levelState[game.player.pos.y - 1][game.player.pos.x]];
-	}
-	function getTileLeft() {
-		return mapCodes[game.levelState[game.player.pos.y][game.player.pos.x - 1]];
-	}
-	function getTileRight() {
-		return mapCodes[game.levelState[game.player.pos.y][game.player.pos.x + 1]];
-	}
-	function getTileBottomLeft() {
-		return mapCodes[game.levelState[game.player.pos.y + 1][game.player.pos.x - 1]];
-	}
-	function getTileBottomRight() {
-		return mapCodes[game.levelState[game.player.pos.y + 1][game.player.pos.x + 1]];
-	}
-	function drawTile(tileX, tileY, desX, desY, sX, sY) {
-		if (!sX) {sX = 40}
-		if (!sY) {sY = 40}
-		ctx.drawImage(sprite, tileX * sX, tileY * sY, sX, sY, desX * sX, desY * sY, sX, sY);
-	}
-
-	function drawHero(tileX, tileY, desX, desY, sX, sY) {
-		if (!sX) {sX = 40}
-		if (!sY) {sY = 40}
-		if (game.player.direction == "right") {
-			ctx.drawImage(playerSprite, tileX * sX, tileY * sY, sX, sY, desX * sX, desY * sY, sX, sY);
-		} else {
-			ctx.drawImage(playerSprite, (11 - tileX) * sX, tileY * sY, sX, sY, desX * sX, desY * sY, sX, sY);
-		}
-		
-	}
-
-	function drawMap(map) {
-
-		var i = 0, j = 0;
-
-		for (i = 0; i < 12; i++) {
-			for (j = 0; j < 16; j++) {
-				
-				var mapCode = map[i][j];
-
-				var tileX = mapCodes[mapCode].spriteX;
-				var tileY = mapCodes[mapCode].spriteY;
-
-				drawTile(tileX, tileY, j, i);
-
-				if (mapCode == 3) { // Entrance Portal
-					var startingPos = {
-						x: j,
-						y: i
-					}
+			if (mapCode == 3) { // Entrance Portal
+				var startingPos = {
+					x: j,
+					y: i
 				}
 			}
 		}
-
-		return startingPos;
-
-	}
-  	
-	// Load Tiles
-	var sprite = new Image();
-	sprite.src = 'assets/XYTILES.JPG';
-	var spriteLoaded = false;
-	sprite.onload = function() {
-		spriteLoaded = true;
 	}
 
-	var playerSprite = new Image();
-	playerSprite.src = 'assets/XYHERO.JPG'
-	var playerLoaded = false;
-	playerSprite.onload = function() {
-		playerLoaded = true;
-		if (spriteLoaded) {
-			game.player.pos = drawMap(levels[game.level]);
-			game.levelState = levels[game.level].slice(0, levels[game.level].length);
-			main();
-		}
+	return startingPos;
+
+}
+	
+// Load Tiles
+var sprite = new Image();
+sprite.src = 'assets/XYTILES.JPG';
+var spriteLoaded = false;
+sprite.onload = function() {
+	spriteLoaded = true;
+}
+
+var playerSprite = new Image();
+playerSprite.src = 'assets/XYHERO.JPG'
+var playerLoaded = false;
+playerSprite.onload = function() {
+	playerLoaded = true;
+	if (spriteLoaded) {
+		game.player.pos = drawMap(levels[game.level]);
+		game.levelState = levels[game.level].slice(0, levels[game.level].length);
+		main(0);
+	}
+}
+
+window.addEventListener("keydown", function(e) {
+
+	switch(e.keyCode) {
+		case 32: // Spacebar
+			if (game.player.state == "standing") {
+
+				if (game.keysDown.W && !getTileAbove().blocking) {
+					playerMagicUp()
+				} else if (game.player.direction == "left") {
+					playerMagicLeft();
+				} else {
+					playerMagicRight();
+				}
+
+			} else if (game.player.state == "crouching" && game.player.animationStep > 10) {
+
+				if (game.player.direction == "left") {
+					playerCrouchMagicLeft();
+				} else {
+					playerCrouchMagicRight();
+				}
+
+			}
+			game.keysDown.space = true;
+			break;
+		case 37: // Left Arrow
+		case 65: // A
+			game.keysDown.A = true;
+			playerWalkLeft();
+			break;
+		case 39: // Right Arrow
+		case 68: // D
+			game.keysDown.D = true;
+			playerWalkRight();	
+			break;
+		case 40: // Down Arrow
+		case 83: // S
+			if (game.player.state == "standing") {
+				playerCrouch();
+			}
+			game.keysDown.S = true;
+			break;
+		case 38: // Up Arrow
+		case 87: // W
+			game.keysDown.W = true;
+			break;
 	}
 
-	window.addEventListener("keydown", function(e) {
+}, true);
 
-		switch(e.keyCode) {
-			case 32: // Spacebar
-				if (game.player.state == "standing") {
+window.addEventListener("keyup", function(e) {
 
-					if (game.keysDown.W && !getTileAbove().blocking) {
-						playerMagicUp()
-					} else if (game.player.direction == "left") {
-						playerMagicLeft();
-					} else {
-						playerMagicRight();
-					}
+	switch(e.keyCode) {
+		case 32: // Spacebar
+			game.keysDown.Space = false;
+			break;
+		case 37: // Left Arrow
+		case 65: // A
+			game.keysDown.A = false;
+			break;
+		case 39:
+		case 68:
+			game.keysDown.D = false;
+			break;
+		case 40: // Down Arrow
+		case 83: // S
+			if (game.player.state == "crouching") {
+				playerStand();
+			}
+			game.keysDown.S = false;
+			break;
+		case 38: // Up Arrow
+		case 87: // W
+			game.keysDown.W = false;
+			break;
+	}
 
-				} else if (game.player.state == "crouching" && game.player.animationStep > 10) {
-
-					if (game.player.direction == "left") {
-						playerCrouchMagicLeft();
-					} else {
-						playerCrouchMagicRight();
-					}
-
-				}
-				game.keysDown.space = true;
-				break;
-			case 37: // Left Arrow
-			case 65: // A
-				game.keysDown.A = true;
-				playerWalkLeft();
-				break;
-			case 39: // Right Arrow
-			case 68: // D
-				game.keysDown.D = true;
-				playerWalkRight();	
-				break;
-			case 40: // Down Arrow
-			case 83: // S
-				if (game.player.state == "standing") {
-					playerCrouch();
-				}
-				game.keysDown.S = true;
-				break;
-			case 38: // Up Arrow
-			case 87: // W
-				game.keysDown.W = true;
-				break;
-		}
-
-	}, true);
-
-	window.addEventListener("keyup", function(e) {
-
-		switch(e.keyCode) {
-			case 32: // Spacebar
-				game.keysDown.Space = false;
-				break;
-			case 37: // Left Arrow
-			case 65: // A
-				game.keysDown.A = false;
-				break;
-			case 39:
-			case 68:
-				game.keysDown.D = false;
-				break;
-			case 40: // Down Arrow
-			case 83: // S
-				if (game.player.state == "crouching") {
-					playerStand();
-				}
-				game.keysDown.S = false;
-				break;
-			case 38: // Up Arrow
-			case 87: // W
-				game.keysDown.W = false;
-				break;
-		}
-
-	}, true);
+}, true);
 
 //})();
 
