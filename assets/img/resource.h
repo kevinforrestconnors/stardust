260fda9f4e7dec811dc26d4f9d92f8c1d40f7871
Kevin Connors <connorsk@grinnell.edu>
2017-10-25 14:05:38 -0500

    add windows images

---
+++ assets/img/resource.h
@@ -0,0 +1,57 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by resource.rc
+//
+#define IDD_DIALOG1                     101
+#define IMG_ABOUT                       102
+#define IMG_INSTR                       103
+#define IMG_MAIN                        104
+#define IMG_PLAYERS                     105
+#define IMG_TILES1                      106
+#define IMG_TILES2                      107
+#define IMG_TILES3                      108
+#define IMG_VICTORY                     109
+#define SND_BEGIN                       111
+#define SND_ABOUT                       113
+#define SND_WRONG                       114
+#define SND_JIM                         115
+#define SND_INFO                        116
+#define SND_START                       117
+#define SND_VICTORY                     118
+#define SND_FALL                        119
+#define SND_LEVEL                       120
+#define SND_BYE                         121
+#define SND_PORTAL                      122
+#define SND_ELEVFADE                    123
+#define SND_FIRST_ENTER                 124
+#define SND_WARP                        125
+#define SND_COALS                       126
+#define SND_BLUE                        127
+#define SND_DUD                         128
+#define SND_GREEN                       129
+#define SND_FALLWALL                    130
+#define SND_GREENFADE                   131
+#define SND_LAND                        132
+#define SND_GBYE                        133
+#define SND_TELEPORT                    134
+#define NO_CURSOR                       135
+#define APP_ICON                        137
+#define SND_ENTER                       138
+#define SND_WARPFADE                    139
+#define IMG_WRONG                       140
+#define IMG_CURSOR                      141
+#define IMG_FONT                        142
+#define IMG_LEVEL                       143
+#define IMG_LOAD                        144
+#define IMG_BUTTONS                     145
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        146
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
